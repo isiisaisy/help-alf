c028c3044ecae030dc1b4138d66c67dd6339967e
isabella <if23b159@technikum-wien.at>
2024-05-13 23:55:25 +0200

    Test case 2 implemented, result is failed

--- spec/HelpAlf_spec.js
+++ spec/HelpAlf_spec.js
@@ -4,9 +4,15 @@ describe("Find Spaceship", function () {
   });
 
   // Test case 1
-  it("should find the spaceship and return its coordinates [7, 2]", () => {
+  it("TC1: should find the spaceship and return its coordinates [7, 2]", () => {
     const map =
       "..........\n..........\n..........\n.......X..\n..........\n..........";
     expect(helpalf.FindSpaceship(map)).toEqual([7, 2]);
+  });
+
+  // Test case 2
+  it('TC2: should not find the spaceship and return "Spaceship lost forever"', () => {
+    const map = "";
+    expect(helpAlf.FindSpaceship(map)).toEqual("Spaceship lost forever.");
   });
 });
