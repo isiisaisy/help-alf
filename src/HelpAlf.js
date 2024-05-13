d049cecb7299852cf850a0b8e6da234e0a260956
isabella <if23b159@technikum-wien.at>
2024-05-13 23:49:33 +0200

    HelpAlf function implemented, Test case 1 is succesfull

--- src/HelpAlf.js
+++ src/HelpAlf.js
@@ -1,5 +1,12 @@
 //HelpAlf.js
 function HelpAlf() {}
 HelpAlf.prototype.FindSpaceship = function (map) {
+  const lines = map.split("\n");
+  for (let y = lines.length - 1; y >= 0; y--) {
+    const x = lines[y].indexOf("X");
+    if (x !== -1) {
+      return [x, lines.length - y - 1];
+    }
+  }
   return 0;
 };
